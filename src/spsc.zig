cdee62fe5ca2e3c992d040efc0b141126be48e60
wyzdwdz <wyzdwdz@gmail.com>
2026-07-16 10:09:23 +0200

    refactor: use @This() to define struct

--- src/spsc.zig
+++ src/spsc.zig
@@ -1,122 +1,120 @@
+pub const SpscVarQueue = @This();
+
 const std = @import("std");
 const testing = std.testing;
 
 const cache_line = std.atomic.cache_line;
 
-pub const SpscVarQueue = struct {
-    const Self = @This();
+pub const MsgHeader = struct {
+    size: std.atomic.Value(u32) = .init(0),
+    log_id: u32 = 0,
 
-    pub const MsgHeader = struct {
-        size: std.atomic.Value(u32) = .init(0),
-        log_id: u32 = 0,
+    pub fn push(self: *MsgHeader, sz: u32) void {
+        self.size.store(sz + @sizeOf(MsgHeader), .release);
+    }
 
-        pub fn push(self: *MsgHeader, sz: u32) void {
-            self.size.store(sz + @sizeOf(MsgHeader), .release);
-        }
-
-        pub fn get_tsc(self: *const MsgHeader) i64 {
-            const ptr: [*]const u8 = @ptrCast(self);
-            return std.mem.readInt(
-                i64,
-                @as(*const [8]u8, @ptrCast(ptr + @sizeOf(MsgHeader))),
-                .native,
-            );
-        }
-
-        pub fn set_tsc(self: *const MsgHeader, tsc: i64) void {
-            const ptr: [*]u8 = @ptrCast(@constCast(self));
-            std.mem.writeInt(i64, @as(*[8]u8, @ptrCast(ptr + @sizeOf(MsgHeader))), tsc, .native);
-        }
-    };
-
-    _allocator: std.mem.Allocator,
-
-    _blk: []align(cache_line) MsgHeader,
-
-    _blk_cnt: u32,
-    _write_idx: u32 = 0,
-    _free_write_cnt: u32,
-
-    _read_idx: std.atomic.Value(u32) align(cache_line) = .init(0),
-
-    pub fn init(allocator: std.mem.Allocator, queue_size: u32) !Self {
-        const blk_cnt = queue_size / @sizeOf(MsgHeader);
-        const blk = try allocator.alignedAlloc(
-            MsgHeader,
-            std.mem.Alignment.fromByteUnits(cache_line),
-            blk_cnt,
+    pub fn get_tsc(self: *const MsgHeader) i64 {
+        const ptr: [*]const u8 = @ptrCast(self);
+        return std.mem.readInt(
+            i64,
+            @as(*const [8]u8, @ptrCast(ptr + @sizeOf(MsgHeader))),
+            .native,
         );
-
-        const step = std.heap.pageSize() / @sizeOf(MsgHeader);
-        var i: usize = 0;
-        while (i < blk_cnt) : (i += step) {
-            blk[i].size.store(0, .monotonic);
-        }
-
-        return .{
-            ._allocator = allocator,
-            ._blk = blk,
-            ._blk_cnt = blk_cnt,
-            ._free_write_cnt = blk_cnt,
-        };
     }
 
-    pub fn deinit(self: Self) void {
-        self._allocator.free(self._blk);
-    }
-
-    pub fn alloc(self: *Self, size: u32) ?*MsgHeader {
-        var asize = size;
-        asize += @sizeOf(MsgHeader);
-
-        const blk_sz: u32 = (asize + @sizeOf(MsgHeader) - 1) / @sizeOf(MsgHeader);
-
-        if (blk_sz >= self._free_write_cnt) {
-            const read_idx_cache = self._read_idx.load(.acquire);
-
-            if (read_idx_cache <= self._write_idx) {
-                self._free_write_cnt = self._blk_cnt - self._write_idx;
-
-                if (blk_sz >= self._free_write_cnt and read_idx_cache != 0) {
-                    self._blk[0].size.store(0, .monotonic);
-                    self._blk[self._write_idx].size.store(1, .release);
-                    self._write_idx = 0;
-                    self._free_write_cnt = read_idx_cache;
-                }
-            } else {
-                self._free_write_cnt = read_idx_cache - self._write_idx;
-            }
-
-            if (self._free_write_cnt <= blk_sz) {
-                return null;
-            }
-        }
-
-        const ret = &self._blk[self._write_idx];
-        self._write_idx += blk_sz;
-        self._free_write_cnt -= blk_sz;
-        self._blk[self._write_idx].size.store(0, .monotonic);
-
-        return ret;
-    }
-
-    pub fn front(self: *Self) ?*MsgHeader {
-        var size = self._blk[self._read_idx.load(.monotonic)].size.load(.acquire);
-        if (size == 1) {
-            self._read_idx.store(0, .monotonic);
-            size = self._blk[0].size.load(.monotonic);
-        }
-        if (size == 0) return null;
-        return &self._blk[self._read_idx.load(.monotonic)];
-    }
-
-    pub fn pop(self: *Self) void {
-        const blk_sz = (self._blk[self._read_idx.load(.monotonic)].size.load(.monotonic) +
-            @sizeOf(MsgHeader) - 1) / @sizeOf(MsgHeader);
-
-        _ = self._read_idx.fetchAdd(blk_sz, .release);
+    pub fn set_tsc(self: *const MsgHeader, tsc: i64) void {
+        const ptr: [*]u8 = @ptrCast(@constCast(self));
+        std.mem.writeInt(i64, @as(*[8]u8, @ptrCast(ptr + @sizeOf(MsgHeader))), tsc, .native);
     }
 };
+
+_allocator: std.mem.Allocator,
+
+_blk: []align(cache_line) MsgHeader,
+
+_blk_cnt: u32,
+_write_idx: u32 = 0,
+_free_write_cnt: u32,
+
+_read_idx: std.atomic.Value(u32) align(cache_line) = .init(0),
+
+pub fn init(allocator: std.mem.Allocator, queue_size: u32) !SpscVarQueue {
+    const blk_cnt = queue_size / @sizeOf(MsgHeader);
+    const blk = try allocator.alignedAlloc(
+        MsgHeader,
+        std.mem.Alignment.fromByteUnits(cache_line),
+        blk_cnt,
+    );
+
+    const step = std.heap.pageSize() / @sizeOf(MsgHeader);
+    var i: usize = 0;
+    while (i < blk_cnt) : (i += step) {
+        blk[i].size.store(0, .monotonic);
+    }
+
+    return .{
+        ._allocator = allocator,
+        ._blk = blk,
+        ._blk_cnt = blk_cnt,
+        ._free_write_cnt = blk_cnt,
+    };
+}
+
+pub fn deinit(self: SpscVarQueue) void {
+    self._allocator.free(self._blk);
+}
+
+pub fn alloc(self: *SpscVarQueue, size: u32) ?*MsgHeader {
+    var asize = size;
+    asize += @sizeOf(MsgHeader);
+
+    const blk_sz: u32 = (asize + @sizeOf(MsgHeader) - 1) / @sizeOf(MsgHeader);
+
+    if (blk_sz >= self._free_write_cnt) {
+        const read_idx_cache = self._read_idx.load(.acquire);
+
+        if (read_idx_cache <= self._write_idx) {
+            self._free_write_cnt = self._blk_cnt - self._write_idx;
+
+            if (blk_sz >= self._free_write_cnt and read_idx_cache != 0) {
+                self._blk[0].size.store(0, .monotonic);
+                self._blk[self._write_idx].size.store(1, .release);
+                self._write_idx = 0;
+                self._free_write_cnt = read_idx_cache;
+            }
+        } else {
+            self._free_write_cnt = read_idx_cache - self._write_idx;
+        }
+
+        if (self._free_write_cnt <= blk_sz) {
+            return null;
+        }
+    }
+
+    const ret = &self._blk[self._write_idx];
+    self._write_idx += blk_sz;
+    self._free_write_cnt -= blk_sz;
+    self._blk[self._write_idx].size.store(0, .monotonic);
+
+    return ret;
+}
+
+pub fn front(self: *SpscVarQueue) ?*MsgHeader {
+    var size = self._blk[self._read_idx.load(.monotonic)].size.load(.acquire);
+    if (size == 1) {
+        self._read_idx.store(0, .monotonic);
+        size = self._blk[0].size.load(.monotonic);
+    }
+    if (size == 0) return null;
+    return &self._blk[self._read_idx.load(.monotonic)];
+}
+
+pub fn pop(self: *SpscVarQueue) void {
+    const blk_sz = (self._blk[self._read_idx.load(.monotonic)].size.load(.monotonic) +
+        @sizeOf(MsgHeader) - 1) / @sizeOf(MsgHeader);
+
+    _ = self._read_idx.fetchAdd(blk_sz, .release);
+}
 
 test "wrap alloc" {
     const block_size = @sizeOf(SpscVarQueue.MsgHeader);
--- src/tscns.zig
+++ src/tscns.zig
@@ -1,3 +1,5 @@
+pub const TscNs = @This();
+
 const std = @import("std");
 const builtin = @import("builtin");
 
@@ -37,206 +39,202 @@ const Atomic64 = struct {
     }
 };
 
-pub const TscNs = struct {
-    const Self = @This();
+_io: std.Io,
 
-    _io: std.Io,
+_param_seq: std.atomic.Value(u32) align(cache_line) = .init(0),
+_ns_per_tsc: Atomic64 = .init(@as(f64, 0.0)),
+_base_tsc: Atomic64 = .init(@as(i64, 0)),
+_base_ns: Atomic64 = .init(@as(i64, 0)),
+_calibrate_interval_ns: i64,
+_base_ns_err: i64 = 0,
+_next_calibrate_tsc: i64 = 0,
 
-    _param_seq: std.atomic.Value(u32) align(cache_line) = .init(0),
-    _ns_per_tsc: Atomic64 = .init(@as(f64, 0.0)),
-    _base_tsc: Atomic64 = .init(@as(i64, 0)),
-    _base_ns: Atomic64 = .init(@as(i64, 0)),
-    _calibrate_interval_ns: i64,
-    _base_ns_err: i64 = 0,
-    _next_calibrate_tsc: i64 = 0,
+const NsPerSec = 1_000_000_000;
 
-    const NsPerSec = 1_000_000_000;
+pub const Config = struct {
+    init_calibrate_ns: i64 = 20_000_000,
+    calibrate_interval_ns: i64 = 3_000_000_000,
+};
 
-    pub const Config = struct {
-        init_calibrate_ns: i64 = 20_000_000,
-        calibrate_interval_ns: i64 = 3_000_000_000,
+pub fn init(io: std.Io, config: Config) !TscNs {
+    var tscns = TscNs{
+        ._io = io,
+        ._calibrate_interval_ns = config.calibrate_interval_ns,
     };
 
-    pub fn init(io: std.Io, config: Config) !Self {
-        var tscns = Self{
-            ._io = io,
-            ._calibrate_interval_ns = config.calibrate_interval_ns,
-        };
+    const base = tscns.syncTime();
+    const expire_ns = base.ns_out + config.init_calibrate_ns;
 
-        const base = tscns.syncTime();
-        const expire_ns = base.ns_out + config.init_calibrate_ns;
-
-        while (tscns.rdsysns() < expire_ns) {
-            std.atomic.spinLoopHint();
-        }
-
-        const delay = tscns.syncTime();
-
-        const init_ns_per_tsc = @as(f64, @floatFromInt(delay.ns_out - base.ns_out)) /
-            @as(f64, @floatFromInt(delay.tsc_out - base.tsc_out));
-
-        tscns.saveParam(base.tsc_out, base.ns_out, 0, init_ns_per_tsc);
-
-        return tscns;
+    while (tscns.rdsysns() < expire_ns) {
+        std.atomic.spinLoopHint();
     }
 
-    pub fn calibrate(self: *Self) void {
-        if (self.rdtsc() < self._next_calibrate_tsc) return;
-        const time = self.syncTime();
-        var ns_err = self.tsc2ns(time.tsc_out) - time.ns_out;
-        if (ns_err > 1_000_000) ns_err = 1_000_000;
-        if (ns_err < -1_000_000) ns_err = -1_000_000;
+    const delay = tscns.syncTime();
 
-        const ns_per_tsc = self._ns_per_tsc.load(f64, .monotonic);
-        const base_tsc = self._base_tsc.load(i64, .monotonic);
-        const new_ns_per_tsc = ns_per_tsc * (1.0 -
-            @as(f64, @floatFromInt(ns_err + ns_err - self._base_ns_err)) /
-                (@as(f64, @floatFromInt(time.tsc_out - base_tsc)) * ns_per_tsc));
+    const init_ns_per_tsc = @as(f64, @floatFromInt(delay.ns_out - base.ns_out)) /
+        @as(f64, @floatFromInt(delay.tsc_out - base.tsc_out));
 
-        self.saveParam(time.tsc_out, time.ns_out, ns_err, new_ns_per_tsc);
-    }
+    tscns.saveParam(base.tsc_out, base.ns_out, 0, init_ns_per_tsc);
 
-    pub fn rdns(self: *Self) i64 {
-        return self.tsc2ns(self.rdtsc());
-    }
+    return tscns;
+}
 
-    pub fn rdtsc(self: Self) i64 {
-        switch (builtin.cpu.arch) {
-            .x86, .x86_64 => {
-                var lo: u32 = undefined;
-                var hi: u32 = undefined;
+pub fn calibrate(self: *TscNs) void {
+    if (self.rdtsc() < self._next_calibrate_tsc) return;
+    const time = self.syncTime();
+    var ns_err = self.tsc2ns(time.tsc_out) - time.ns_out;
+    if (ns_err > 1_000_000) ns_err = 1_000_000;
+    if (ns_err < -1_000_000) ns_err = -1_000_000;
 
-                asm volatile ("rdtsc"
-                    : [lo] "={eax}" (lo),
-                      [hi] "={edx}" (hi),
-                );
+    const ns_per_tsc = self._ns_per_tsc.load(f64, .monotonic);
+    const base_tsc = self._base_tsc.load(i64, .monotonic);
+    const new_ns_per_tsc = ns_per_tsc * (1.0 -
+        @as(f64, @floatFromInt(ns_err + ns_err - self._base_ns_err)) /
+            (@as(f64, @floatFromInt(time.tsc_out - base_tsc)) * ns_per_tsc));
 
-                return (@as(i64, hi) << 32 | @as(i64, lo));
-            },
-            .aarch64, .aarch64_be => {
-                var val: u64 = undefined;
+    self.saveParam(time.tsc_out, time.ns_out, ns_err, new_ns_per_tsc);
+}
 
-                asm volatile ("mrs %[val], cntvct_el0"
-                    : [val] "=r" (val),
-                );
+pub fn rdns(self: *TscNs) i64 {
+    return self.tsc2ns(self.rdtsc());
+}
 
-                return @as(i64, @bitCast(val));
-            },
-            .riscv64, .riscv64be => {
-                var val: u64 = undefined;
+pub fn rdtsc(self: TscNs) i64 {
+    switch (builtin.cpu.arch) {
+        .x86, .x86_64 => {
+            var lo: u32 = undefined;
+            var hi: u32 = undefined;
 
-                asm volatile ("rdtime %[val]"
-                    : [val] "=r" (val),
-                );
-
-                return @as(i64, @bitCast(val));
-            },
-            .loongarch64 => {
-                var val: u64 = undefined;
-
-                asm volatile ("rdtime.d %[val], $zero"
-                    : [val] "=r" (val),
-                );
-
-                return @as(i64, @bitCast(val));
-            },
-            .powerpc64, .powerpc64le => {
-                var val: u64 = undefined;
-
-                asm volatile ("mftb %[val]"
-                    : [val] "=r" (val),
-                );
-
-                return @as(i64, @bitCast(val));
-            },
-            else => {
-                return self.rdsysns();
-            },
-        }
-    }
-
-    pub fn tsc2ns(self: *Self, tsc: i64) i64 {
-        while (true) {
-            const before_seq = self._param_seq.load(.acquire) & ~@as(u32, 1);
-
-            const ns_per_tsc = self._ns_per_tsc.load(f64, .monotonic);
-            const base_ns = self._base_ns.load(i64, .monotonic);
-            const base_tsc = self._base_tsc.load(i64, .monotonic);
-            const ns = base_ns + @as(
-                i64,
-                @trunc(@as(f64, @floatFromInt(tsc - base_tsc)) * ns_per_tsc),
+            asm volatile ("rdtsc"
+                : [lo] "={eax}" (lo),
+                  [hi] "={edx}" (hi),
             );
 
-            const after_seq = self._param_seq.load(.acquire);
-            if (before_seq == after_seq) return ns;
-        }
+            return (@as(i64, hi) << 32 | @as(i64, lo));
+        },
+        .aarch64, .aarch64_be => {
+            var val: u64 = undefined;
+
+            asm volatile ("mrs %[val], cntvct_el0"
+                : [val] "=r" (val),
+            );
+
+            return @as(i64, @bitCast(val));
+        },
+        .riscv64, .riscv64be => {
+            var val: u64 = undefined;
+
+            asm volatile ("rdtime %[val]"
+                : [val] "=r" (val),
+            );
+
+            return @as(i64, @bitCast(val));
+        },
+        .loongarch64 => {
+            var val: u64 = undefined;
+
+            asm volatile ("rdtime.d %[val], $zero"
+                : [val] "=r" (val),
+            );
+
+            return @as(i64, @bitCast(val));
+        },
+        .powerpc64, .powerpc64le => {
+            var val: u64 = undefined;
+
+            asm volatile ("mftb %[val]"
+                : [val] "=r" (val),
+            );
+
+            return @as(i64, @bitCast(val));
+        },
+        else => {
+            return self.rdsysns();
+        },
     }
+}
 
-    fn rdsysns(self: Self) i64 {
-        return @intCast(std.Io.Timestamp.now(self._io, .real).toNanoseconds());
-    }
+pub fn tsc2ns(self: *TscNs, tsc: i64) i64 {
+    while (true) {
+        const before_seq = self._param_seq.load(.acquire) & ~@as(u32, 1);
 
-    fn syncTime(self: Self) struct { tsc_out: i64, ns_out: i64 } {
-        const n = if (builtin.os.tag == .windows) 15 else 3;
-
-        var tsc: [n + 1]i64 = undefined;
-        var ns: [n + 1]i64 = undefined;
-
-        tsc[0] = self.rdtsc();
-        for (1..n + 1) |i| {
-            ns[i] = self.rdsysns();
-            tsc[i] = self.rdtsc();
-        }
-
-        var j: usize = 0;
-
-        if (builtin.os.tag == .windows) {
-            j = 1;
-            for (2..n + 1) |i| {
-                if (ns[i] == ns[i - 1]) continue;
-                tsc[j - 1] = tsc[i - 1];
-                ns[j] = ns[i];
-                j += 1;
-            }
-            j -= 1;
-        } else {
-            j = n + 1;
-        }
-
-        var best: usize = 1;
-
-        for (2..j) |i| {
-            if (tsc[i] - tsc[i - 1] < tsc[best] - tsc[best - 1]) best = i;
-        }
-
-        return .{ .tsc_out = (tsc[best] + tsc[best - 1]) >> 1, .ns_out = ns[best] };
-    }
-
-    fn saveParam(
-        self: *Self,
-        base_tsc: i64,
-        sys_ns: i64,
-        base_ns_err: i64,
-        new_ns_per_tsc: f64,
-    ) void {
-        self._base_ns_err = base_ns_err;
-        self._next_calibrate_tsc = base_tsc + @as(
+        const ns_per_tsc = self._ns_per_tsc.load(f64, .monotonic);
+        const base_ns = self._base_ns.load(i64, .monotonic);
+        const base_tsc = self._base_tsc.load(i64, .monotonic);
+        const ns = base_ns + @as(
             i64,
-            @trunc(@as(
-                f64,
-                @floatFromInt(self._calibrate_interval_ns - 1000),
-            ) / new_ns_per_tsc),
+            @trunc(@as(f64, @floatFromInt(tsc - base_tsc)) * ns_per_tsc),
         );
 
-        _ = self._param_seq.fetchAdd(1, .acquire);
-
-        self._base_tsc.store(base_tsc, .monotonic);
-        self._base_ns.store(sys_ns + base_ns_err, .monotonic);
-        self._ns_per_tsc.store(new_ns_per_tsc, .monotonic);
-
-        _ = self._param_seq.fetchAdd(1, .release);
+        const after_seq = self._param_seq.load(.acquire);
+        if (before_seq == after_seq) return ns;
     }
-};
+}
+
+fn rdsysns(self: TscNs) i64 {
+    return @intCast(std.Io.Timestamp.now(self._io, .real).toNanoseconds());
+}
+
+fn syncTime(self: TscNs) struct { tsc_out: i64, ns_out: i64 } {
+    const n = if (builtin.os.tag == .windows) 15 else 3;
+
+    var tsc: [n + 1]i64 = undefined;
+    var ns: [n + 1]i64 = undefined;
+
+    tsc[0] = self.rdtsc();
+    for (1..n + 1) |i| {
+        ns[i] = self.rdsysns();
+        tsc[i] = self.rdtsc();
+    }
+
+    var j: usize = 0;
+
+    if (builtin.os.tag == .windows) {
+        j = 1;
+        for (2..n + 1) |i| {
+            if (ns[i] == ns[i - 1]) continue;
+            tsc[j - 1] = tsc[i - 1];
+            ns[j] = ns[i];
+            j += 1;
+        }
+        j -= 1;
+    } else {
+        j = n + 1;
+    }
+
+    var best: usize = 1;
+
+    for (2..j) |i| {
+        if (tsc[i] - tsc[i - 1] < tsc[best] - tsc[best - 1]) best = i;
+    }
+
+    return .{ .tsc_out = (tsc[best] + tsc[best - 1]) >> 1, .ns_out = ns[best] };
+}
+
+fn saveParam(
+    self: *TscNs,
+    base_tsc: i64,
+    sys_ns: i64,
+    base_ns_err: i64,
+    new_ns_per_tsc: f64,
+) void {
+    self._base_ns_err = base_ns_err;
+    self._next_calibrate_tsc = base_tsc + @as(
+        i64,
+        @trunc(@as(
+            f64,
+            @floatFromInt(self._calibrate_interval_ns - 1000),
+        ) / new_ns_per_tsc),
+    );
+
+    _ = self._param_seq.fetchAdd(1, .acquire);
+
+    self._base_tsc.store(base_tsc, .monotonic);
+    self._base_ns.store(sys_ns + base_ns_err, .monotonic);
+    self._ns_per_tsc.store(new_ns_per_tsc, .monotonic);
+
+    _ = self._param_seq.fetchAdd(1, .release);
+}
 
 test "tsc2ns retry" {
     const testing = std.testing;
